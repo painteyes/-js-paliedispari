f2024d260e55b3f049b7032b38789ba099cfedd3
painteyes <andreantoniocchipinti@gmail.com>
2021-11-22 16:21:15 +0100

    commit changes

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,6 +1,9 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+
+// SOLUTION 
+
 // Get a word from user
 let userWord = prompt('Inserisci una parola');
 
@@ -25,17 +28,60 @@ function isPalindrome (WordToCheck) {
     let thisWordBackwards = [];
 
     for (let i = 0; i < WordToCheck.length; i++) {
-        thisWord.push(userWord[i]);
+        thisWord.push(WordToCheck[i]);
     }
 
-    for (let i = WordToCheck.length - 1; i = 0; i--) {
-        thisWordBackwards.push(userWord[i]);
+    for (let i = WordToCheck.length - 1; i >= 0; i--) {
+        thisWordBackwards.push(WordToCheck[i]);
     }
-
-    if (thisWordBackwards === thisWord) {
-        palindromeFound = True; 
+    
+    if (thisWordBackwards.toString() === thisWord.toString()) {
+        palindromeFound = true; 
     } 
 
     return palindromeFound;
 }
+
+//  ALTERNATIVE SOLUTION
+
+// Get a word from user
+let userWordAlternative = prompt('Inserisci una parola');
+
+// Check the word using function
+let resultAlternative = isPalindromeAlternative (userWord);
+
+// Generate the message to the user
+if (result) {
+    alert ('Questa parola è palindroma');
+} else {
+    alert ('Questa parola non è palindroma');
+}
+
+// Function to check if a word is palindrome
+function isPalindromeAlternative (WordToCheck) {
+
+    // Default flag variable
+    let palindromeFound = false;
+
+    // Arrays that will contain the letters of the word forward and backward
+    let thisWord = '';
+    let thisWordBackwards = '';
+
+    for (let i = 0; i < WordToCheck.length; i++) {
+        WordToCheck[i] += thisWord;
+    }
+
+    for (let i = WordToCheck.length - 1; i >= 0; i--) {
+        WordToCheck[i] += thisWordBackwards;
+    }
+
+    if (thisWordBackwards === thisWord) {
+        palindromeFound = true; 
+    } 
+
+    return palindromeFound;
+}
+
+
+
 
